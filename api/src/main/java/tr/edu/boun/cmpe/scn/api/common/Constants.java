09cabd47078440084a6d8fcd4c9ea76c44cd69f5
esin.karaman <esin.karaman@argela.com.tr>
2017-06-04 15:11:56 +0300

    API - Constants and Tool updated

--- api/src/main/java/tr/edu/boun/cmpe/scn/api/common/Constants.java
+++ api/src/main/java/tr/edu/boun/cmpe/scn/api/common/Constants.java
@@ -8,5 +8,5 @@ public class Constants {
     public static final int SCN_SERVICE_PORT = 1743;
     public static final String SCN_BROADCAST_ADDRESS = "255.255.255.255";
     public static final String UTF8 = "UTF-8";
-    public static final int PACKET_SIZE = 4096;
+    public static final int PACKET_SIZE = 4096 * 3;
 }
--- api/src/main/java/tr/edu/boun/cmpe/scn/api/common/Tool.java
+++ api/src/main/java/tr/edu/boun/cmpe/scn/api/common/Tool.java
@@ -1,9 +1,17 @@
 package tr.edu.boun.cmpe.scn.api.common;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Created by esinka on 2/4/2017.
  */
 public class Tool {
+
+    public static final String TIMESTAMP_PATTERN = "yyyy-MM-dd HH:mm:ss.SSS";
+    public static final String ELAPSED_TIME_SEP = ";";
 
     public static void checkNull(Object arg, String message) {
         if (arg == null) {
@@ -15,5 +23,41 @@ public class Tool {
         if(!expressionValue) {
             throw new IllegalArgumentException(message);
         }
+    }
+
+    public static int generateRandomInteger(int aStart, int aEnd, Random aRandom){
+        if (aStart > aEnd) {
+            throw new IllegalArgumentException("Start cannot exceed End.");
+        }
+        //get the range, casting to long to avoid overflow problems
+        long range = (long)aEnd - (long)aStart + 1;
+        // compute a fraction of the range, 0 <= frac < range
+        long fraction = (long)(range * aRandom.nextDouble());
+        return (int)(fraction + aStart);
+    }
+
+    public static int getRandomlyFromRange(int rangeStart, int rangeEndInclusive) {
+        return ThreadLocalRandom.current().nextInt(rangeStart, rangeEndInclusive + 1);
+    }
+
+    public static String elapsedLogForClient(long elapsed, String ipAddress) {
+        SimpleDateFormat sdf = new SimpleDateFormat(TIMESTAMP_PATTERN);
+        return new StringBuilder()
+                .append(sdf.format(new Date())).append(ELAPSED_TIME_SEP)
+                .append(ipAddress).append(ELAPSED_TIME_SEP)
+                .append(elapsed).toString();
+    }
+
+    public static String logForService(String serviceIp, String clientIp) {
+        SimpleDateFormat sdf = new SimpleDateFormat(TIMESTAMP_PATTERN);
+        return new StringBuilder()
+                .append(sdf.format(new Date())).append(ELAPSED_TIME_SEP)
+                .append(serviceIp).append(ELAPSED_TIME_SEP)
+                .append(clientIp).toString();
+    }
+
+    public static String formatTime() {
+        SimpleDateFormat format = new SimpleDateFormat(TIMESTAMP_PATTERN);
+        return format.format(System.currentTimeMillis());
     }
 }
